3425476f8274f7407fe6a98b3246d7957ff8bc80
Victor Fernandes <victor.magalhaes@klickpages.com.br>
2020-08-24 10:26:09 -0300

    fix(logger): habilita debugger logger

--- src/utils/debugger_logger.ts
+++ src/utils/debugger_logger.ts
@@ -1,8 +1,6 @@
 import debug from 'debug';
 import { logDebugParams } from '../interfaces/IDebuggerLogger';
 
-debug.disable();
-
 export default ({ context, message, metadata = {} }: logDebugParams): void => {
   const debugMessage = {
     message,
